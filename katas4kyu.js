41b01dd8b352e2c310c86012a5ca819b757d6ab4
Jose Javier Sanahuja <82359964+josejaviersanahuja@users.noreply.github.com>
2023-01-01 19:36:29 +0000

    add sudoku validation

--- katas4kyu.js
+++ katas4kyu.js
@@ -431,3 +431,73 @@ function factorial(n){
 }
 
 console.log(multiply(120,6)); // 120
+
+/**
+ * Given a Sudoku data structure with size NxN, N > 0 and √N == integer, write a method to validate if it has been filled out correctly.
+
+The data structure is a multi-dimensional Array, i.e:
+
+[
+  [7,8,4,  1,5,9,  3,2,6],
+  [5,3,9,  6,7,2,  8,4,1],
+  [6,1,2,  4,3,8,  7,5,9],
+  
+  [9,2,8,  7,1,5,  4,6,3],
+  [3,5,7,  8,4,6,  1,9,2],
+  [4,6,1,  9,2,3,  5,8,7],
+  
+  [8,7,6,  3,9,4,  2,1,5],
+  [2,4,3,  5,6,1,  9,7,8],
+  [1,9,5,  2,8,7,  6,3,4]
+]
+Rules for validation
+
+Data structure dimension: NxN where N > 0 and √N == integer
+Rows may only contain integers: 1..N (N included)
+Columns may only contain integers: 1..N (N included)
+'Little squares' (3x3 in example above) may also only contain integers: 1..N (N included)
+ * @param {Number[][]} data 
+ * @returns {Boolean}
+ */
+var Sudoku = function(data) 
+{
+  //   Private methods
+  // -------------------------
+  const n = data.length
+  const sqr = Math.sqrt(n)
+  const isValid = (arr) => {
+    if (!arr.every(x => typeof x === 'number')) return false
+    const set = new Set(arr)
+    return set.size === n && Math.min(...set) === 1 && Math.max(...set) === n
+  }
+  const isValidRow = (row) => isValid(data[row])
+  const isValidCol = (col) => isValid(data.map(row => row[col]))
+  const isValidSqr = (row, col) => {
+    const arr = []
+    for (let i = row; i < row + sqr; i++) {
+      for (let j = col; j < col + sqr; j++) {
+        arr.push(data[i][j])
+      }
+    }
+    return isValid(arr)
+  }
+
+  //   Public methods
+  // -------------------------
+  return {
+    isValid: function() {
+      // YOUR SOLUTION
+      for (let i = 0; i < n; i++) {
+        if (!isValidRow(i) || !isValidCol(i)) return false
+      }
+      for (let i = 0; i < n; i += sqr) {
+        for (let j = 0; j < n; j += sqr) {
+          if (!isValidSqr(i, j)) return false
+        }
+      }
+      return true
+    }
+  };
+};
+
+console.log(Sudoku([ [ true ] ]).isValid());
